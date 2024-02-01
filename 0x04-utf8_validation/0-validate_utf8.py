bf4594fbe4166c1a21eda3a0796dd67e118c9d09
Kemboi-73 <kemboimichael52@gmail.com>
2024-02-01 14:27:38 +0000

    a method that determines if a given data set represents a valid UTF-8 encoding.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,58 @@
 #!/usr/bin/python3
 """
 Defines a function that determines whether a list of
-integers passed into it conforms
-to UTF-8 format.
+integers passed into it conforms to UTF-8 format.
 """
+from itertools import takewhile
+
+
+def int_to_bits(nums):
+    """
+    Helper function
+    Converts integers to binary representations.
+    """
+    for num in nums:
+        bits = []
+        mask = 1 << 8  # Because there are 8 bits per byte (adds up to 11111111)
+        while mask:
+            mask >>= 1
+            bits.append(bool(num & mask))
+        yield bits
+
+
 def validUTF8(data):
     """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
+    Takes a list of integers and returns True if the list is
+    a valid UTF-8 encoding, else returns False.
     Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
+        data : List of integers representing a possible UTF-8 encoding
+    Returns:
+        bool : True if valid UTF-8, False otherwise
     """
-    num_bytes = 0
-    
-    for num in data:
-        if num_bytes == 0:
-            if (num >> 7) == 0b0:
-                continue
-            elif (num >> 5) == 0b110:
-                num_bytes = 1
-            elif (num >> 4) == 0b1110:
-                num_bytes = 2
-            elif (num >> 3) == 0b11110:
-                num_bytes = 3
-            else:
+    bits = int_to_bits(data)
+
+    for byte in bits:
+        # If single-byte character, it's valid. Continue.
+        if byte[0] == 0:
+            continue
+
+        # If here, the byte is a multi-byte character.
+        ones = sum(takewhile(bool, byte))
+
+        # Check if the number of leading ones is within valid UTF-8 range.
+        if ones <= 1 or ones >= 4:  # UTF-8 can be 1 to 4 bytes long
+            return False
+
+        # Check the next bytes in the sequence for the expected pattern.
+        for _ in range(ones - 1):
+            try:
+                byte = next(bits)
+            except StopIteration:
                 return False
-        else:
-            if (num >> 6) != 0b10:
+
+            # Check if the byte starts with "10" in binary.
+            if byte[0:2] != [1, 0]:
                 return False
-            num_bytes -= 1
-    
-    return num_bytes == 0
+
+    return True
 
